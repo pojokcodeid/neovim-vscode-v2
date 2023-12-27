b30e5dac61550139eca12af1827de0df50a11e99
asep komarudin <pojokcodeid@gmail.com>
2023-12-27 16:12:53 +0700

    first commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+if vim.g.vscode then
+  -- config vscode
+  Map = vim.keymap.set
+  Cmd = vim.cmd
+  VSCodeNotify = vim.fn.VSCodeNotify
+  VSCodeCall = vim.fn.VSCodeCall
+
+  require("vscode.functions")
+  require("vscode.mappings")
+else
+  -- config neovim disini
+end
